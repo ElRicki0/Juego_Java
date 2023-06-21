b6e24c2a0066a469695da5b5af8d38ae04152fc4
ElRicki0 <ricardo06melara@gmail.com>
2023-06-20 20:32:10 -0600

    Se añadio una clase para la pantalla de la ventana con un ciclo de prueva

--- src/JuegoTetris/Pantalla.java
+++ src/JuegoTetris/Pantalla.java
@@ -1,9 +1,39 @@
 package JuegoTetris;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
 /**
  *
  * @author rnmel
  */
-public class Pantalla {
+public class Pantalla extends JPanel{
+    
+    private Timer ciclo;
+    
+    public Pantalla(){
+        
+    ciclo = new Timer(500, new ActionListener() {
+        int n = 0;
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            System.out.println(n++);
+        }
+    });          
+       ciclo.start();
+            
+            }
+            
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawRect(10, 10, 200, 200);
+        
+    }
         
 }
--- src/JuegoTetris/Ventana.java
+++ src/JuegoTetris/Ventana.java
@@ -1,11 +1,13 @@
 package JuegoTetris;
 
+import java.awt.Graphics;
 import javax.swing.JFrame;
 
 public class Ventana {
     
     public static final int WIDTH =445, HEIGHT = 629;  
     
+    private Pantalla pantalla;
     private JFrame ventana;
             
     public Ventana(){
@@ -14,6 +16,9 @@ public class Ventana {
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setResizable(false);
         ventana.setLocationRelativeTo(null);
+        
+        pantalla = new Pantalla();
+        ventana.add(pantalla);
         ventana.setVisible(true);
     }        
     
