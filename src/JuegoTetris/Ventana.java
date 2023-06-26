31333efdc49683b61db170cd5451ce4023bc29a0
kvnro <kvnro@Hardvest>
2023-06-26 06:35:11 -0600

    Agregado de un nuevo Package y edicion en Ventana.java

--- src/JuegoTetris/Ventana.java
+++ src/JuegoTetris/Ventana.java
@@ -1,29 +1,46 @@
-    package JuegoTetris;
+package JuegoTetris;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 
 public class Ventana {
-    
-    public static final int WIDTH =445, HEIGHT = 629;  
-    
+
+    public static final int WIDTH = 445, HEIGHT = 629;
+
     private Pantalla pantalla;
     private JFrame ventana;
-            
-    public Ventana(){
+
+    public Ventana() {
         JFrame ventana = new JFrame("Tetris RN");
         ventana.setSize(WIDTH, HEIGHT);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ventana.setResizable(false);
         ventana.setLocationRelativeTo(null);
-        
+
         pantalla = new Pantalla();
         ventana.add(pantalla);
         ventana.addKeyListener(pantalla);
         ventana.setVisible(true);
-    }        
-    
+
+        reproducirMusica("/Musica/Tetris_Theme_Song.wav"); 
+    }
+
     public static void main(String[] args) {
-        new  Ventana();
-}
+        new Ventana();
+    }
+
+    public void reproducirMusica(String archivo) {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(getClass().getResourceAsStream(archivo));
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
